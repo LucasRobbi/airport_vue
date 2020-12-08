c9a28d3f6598fe53d00e71825e72f47f8118288c
Robbi <lucas.robbi@hotmail.com>
2020-12-08 10:50:40 -0300

    Mais alterações e implementação de função vue
    Signed-off-by: Robbi <lucas.robbi@hotmail.com>

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
  new Vue({
-    el: "#app",
+    el: "#vue-app",
     data: {
-
+      adultos: 0,
+      criancas: 0,
     },
     methods:{
-        
+      add_adulto: function(adultos){
+        this.adultos++;
+      },
+      remove_adulto:function(adultos){
+        this.adultos--;
+      },
+      add_crianca:function(criancas){
+        this.criancas++;
+      },
+      remove_crianca:function(criancas){
+        this.criancas--;
+      },
     },
     computed:{
         
