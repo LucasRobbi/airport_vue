ff72046037e762dd43d519e5d0877411d2248070
JoaoCCM <joaocarlosc.m@hotmail.com>
2020-12-10 19:47:19 -0300

    fixes

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+$('.div-imgs').slick({
+    dots: true,
+    centerMode: true,
+    slidesToShow: 3,
+    // nextArrow: "<img class='arrow-right' src='../src/img/next_arrow.png'>",
+    // prevArrow: "<img class='arrow-left' src='../src/img/prev_arrow.png'>"
+
+});
