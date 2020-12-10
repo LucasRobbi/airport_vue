68fccd944c86ea2a4dcda95f2ba69da4ad940ecd
Beatriz Miranda <beatriz.miranda@acct.global>
2020-12-09 21:42:35 -0300

    fix: quantity stepper

--- app.js
+++ app.js
@@ -1,24 +1,22 @@
- new Vue({
-    el: "#vue-app",
-    data: {
-      adultos: 0,
-      criancas: 0,
+new Vue({
+  el: "#vue-app",
+  data: {
+    adultos: 0,
+    criancas: 0,
+  },
+  methods: {
+    add_adulto() {
+      this.adultos++;
     },
-    methods:{
-      add_adulto: function(adultos){
-        this.adultos++;
-      },
-      remove_adulto:function(adultos){
-        this.adultos--;
-      },
-      add_crianca:function(criancas){
-        this.criancas++;
-      },
-      remove_crianca:function(criancas){
-        this.criancas--;
-      },
+    remove_adulto() {
+      if (this.adultos >= 1) this.adultos--;
     },
-    computed:{
-        
-    }
-  });
+    add_crianca() {
+      this.criancas++;
+    },
+    remove_crianca() {
+      if (this.criancas >= 1) this.criancas--;
+    },
+  },
+  computed: {},
+});
