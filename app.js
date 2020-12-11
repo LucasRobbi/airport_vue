7cd25f43ac615c86144279167bc9b243ccd7ada6
Beatriz Miranda <beatriz.miranda@acct.global>
2020-12-10 21:42:45 -0300

    header vue

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
+const HeaderComponent = Vue.extend({
+  data: () => {},
+  template: "#custom-header"
+});
+
+Vue.component('header-component', {
+  template:`
+    <header>
+      <nav id="menu">
+          <div class="logo">
+              <a href="/pages/index.html" id="inicio"><img class="logo" src="../src/img/inicio.ico"></a>
+          </div>
+          <div class="links">
+              <a href="/pages/minhas_viagens.html" id="minhas_viagens"><img
+                      src="../src/img/icon/iconViagens.svg">Minhas
+                  Viagens</a>
+              <a href="/pages/minha_conta.html" id="minha_conta" class="w3-rigth"><img
+                      src="../src/img/icon/iconUser.svg">Minha Conta</a>
+          </div>
+      </nav>
+    </header>
+  `,
+})
+
 new Vue({
   el: "#vue-app",
   data: {
@@ -17,6 +41,10 @@ new Vue({
     remove_crianca() {
       if (this.criancas >= 1) this.criancas--;
     },
-  },
-  computed: {},
+  }
+
 });
+
+
+
+Vue.config.devtools = true;
