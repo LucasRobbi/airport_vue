61fec750e440bb7dea274cb1102fb91827f0e126
JoaoCCM <joaocarloscm7@gmail.com>
2020-12-12 14:33:06 -0200

    my account page

--- app.js
+++ app.js
@@ -3,6 +3,36 @@ const HeaderComponent = Vue.extend({
   template: "#custom-header"
 });
 
+Vue.component('my-account-component', {
+  template: `
+    <div class="account-card">
+      <div class="destination">
+        <span class='dest'>São Paulo</span>
+        <img src="../src/img/icon/go_to.svg" alt="airline" />
+        <span class='dest'>São Paulo</span>
+      </div>
+      <div class="airline">
+        <img src="../src/img/icon/latam.svg" alt="airline" class="airline-img"/>
+        <span class="latam">Latam</span>
+      </div>
+      <div class="embarque">
+        <span class="embarque-texto">Embarque as: <span class='response'>01h30m</span></span>
+        <span class="embarque-texto">Embarque no dia: <span class='response'>21/08/2021</span></span>
+      </div>
+      <div class="chegada">
+        <span class="embarque-texto">Previsão de chegada: <span class='response'>22/08/2021</span></span>
+      </div>
+      <div class="tempo">
+        <span class="embarque-texto">Tempo estimado: <span class='response'>24h</span></span>
+        <span class="preco">R$ 2000.00</span>
+      </div>
+      <div class="cancelamento">
+        <span class="cancel">Solicitar cancelamento</span>
+      </div>
+    </div>
+  `
+})
+
 Vue.component('header-component', {
   template:`
     <header>
