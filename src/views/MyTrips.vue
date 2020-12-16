213d7de485309f9b80fef67e2b01087b876a0e38
JoaoCCM <joaocarloscm7@gmail.com>
2020-12-16 19:49:09 -0200

    finished purchase component

--- src/views/MyTrips.vue
+++ src/views/MyTrips.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="MyTrips d-flex">
    <h1>Minhas viagens</h1>
-   <MyTrip/>
+   <Purchase/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import MyTrip from '@/components/MyTrip.vue'
+// import MyTrip from '@/components/MyTrip.vue'
+import Purchase from '@/components/Purchase.vue'
 
 export default {
   name: 'MyTrips',
   components: {
-    MyTrip
+    Purchase
   }
 }
 </script>
