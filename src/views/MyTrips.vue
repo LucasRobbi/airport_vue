ff45a8d96923f5222e152053f6281890e4428f4b
JoaoCCM <joaocarloscm7@gmail.com>
2020-12-16 19:51:49 -0200

    fix

--- src/views/MyTrips.vue
+++ src/views/MyTrips.vue
@@ -1,19 +1,18 @@
 <template>
   <div class="MyTrips d-flex">
    <h1>Minhas viagens</h1>
-   <Purchase/>
+   <MyTrip/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-// import MyTrip from '@/components/MyTrip.vue'
-import Purchase from '@/components/Purchase.vue'
+import MyTrip from '@/components/MyTrip.vue'
 
 export default {
   name: 'MyTrips',
   components: {
-    Purchase
+    MyTrip
   }
 }
 </script>
