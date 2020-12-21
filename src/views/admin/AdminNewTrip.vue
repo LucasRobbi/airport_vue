5871d394f0c8cfba05105e30b890751d14072ba8
JoaoCCM <joaocarloscm7@gmail.com>
2020-12-21 21:15:20 -0200

    create flight

--- src/views/admin/AdminNewTrip.vue
+++ src/views/admin/AdminNewTrip.vue
@@ -21,8 +21,8 @@
         </div>
         <div>
           <div class="d-flex">
-            <input class="form-control withBorder mr-4" type="date" placeholder="Data" />
-            <input class="form-control withBorder" type="time" placeholder="Horário" />
+            <input v-model="ship_date" class="form-control withBorder mr-4" type="date" placeholder="Data" />
+            <input v-model="ship_time" class="form-control withBorder" type="time" placeholder="Horário" />
           </div>
           <div class="input-group">
             <div class="input-group-prepend">
@@ -30,7 +30,7 @@
                 <img src="../../assets/icon/time.svg" />
               </span>
             </div>
-            <input type="text" class="form-control" placeholder="Tempo estimado de voo" required />
+            <input v-model="estimated_time" type="text" class="form-control" placeholder="Tempo estimado de voo" required />
           </div>
         </div>
         <div>
@@ -41,6 +41,7 @@
               </span>
             </div>
             <input
+              v-model="limit"
               type="number"
               class="form-control"
               placeholder="Limite de passageiros"
@@ -53,18 +54,54 @@
                 <img src="../../assets/icon/Plane.svg" />
               </span>
             </div>
-            <input type="text" class="form-control" placeholder="Companhia aérea" required />
+            <input v-model="company_name" type="text" class="form-control" placeholder="Companhia aérea" required />
           </div>
         </div>
         <div id="cadastrar">
-          <button class="cadastrar">Cadastrar</button>
+          <button @click='createFlight' class="cadastrar">Cadastrar</button>
         </div>
       </div>
     </div>
   </div>
 </template>
 
-<script></script>
+<script>
+import axios from 'axios';
+
+export default {
+  components: {},
+  data() {
+    return {
+      destination: "Nova Iorc",
+      shipment: "Rio de Janeiro",
+      ship_date: "",
+      ship_time: "",
+      estimated_time: 0,
+      limit: 0,
+      airline_id: 0,
+      status: "",
+      ticket_price: 150.00,
+      image: "",
+      company_name: ""
+    };
+  },
+
+  methods: {
+    createFlight() {
+      const token = sessionStorage.getItem('user_token');
+      axios.get(`http://localhost:5000/airline/name/?name=${ this.company_name }`).then(res => this.airline_id = res.data.id).catch(e => console.error(e))
+      const flight_body = { destination: this.destination, shipment: this.shipment, ship_date: this.ship_date, ship_time: this.ship_time, estimated_time: Number(this.estimated_time), limit: this.limit, airline_id: this.airline_id, status: this.status, ticket_price: this.ticket_price, image: this.image }
+      axios.post('http://localhost:5000/flight', flight_body, { headers: { Authorization: `bearer ${token}` } })
+      .then(res => console.log(res))
+      .catch(e => {
+        if(e.response.status == 401) return this.$swal('Algo deu errado', 'Você não tem permissão para fazer isso', 'warning')
+        if(![200].includes(e.response.status)) return this.$swal('Algo deu errado', 'Tente novamente mais tarde :(', 'error')
+        return this.$swal('Algo deu errado', 'Tente novamente mais tarde :(', 'error')
+      })
+    }
+  },
+}
+</script>
 
 <style>
 #cadastrar {
